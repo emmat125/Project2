4426a10f7a42ce6a281180e01a7b8c9b57aaaac0
Alice Smith <alice@smith.com>
2023-07-05 11:59:40 -0400

    portion of part 3

--- server/server.js
+++ server/server.js
@@ -2,6 +2,23 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const { MongoClient } = require('mongodb');
+var url = "mongodb://localhost:27017";
+const client = new MongoClient(url);
+
+const dbName = "swapi_db";
+const collectionName = "films"
+
+module.exports.call = async function call(operation, parameters, callback) {
+
+await client.connect();
+const db = client.db(dbName);
+const collection = db.collection(collectionName);
+
+console.log( 'call complete: ' + operation );
+    client.close();
+    return 'call complete';
+}
 app.get('/', (req, res) => {
     res.send('Hello World');
 })
